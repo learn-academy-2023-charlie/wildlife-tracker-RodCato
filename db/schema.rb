8374450da724b03b0e60a2f9c3dccbec7de2be88
RodCato <code@eliment.org>
2023-05-26 12:15:38 -0500

    Sighting CRUD Actions complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_26_020010) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_26_170209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_020010) do
     t.string "scientific_binomial"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sightings", force: :cascade do |t|
+    t.string "latitude"
+    t.string "longitude"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "animal_id"
   end
 
 end
